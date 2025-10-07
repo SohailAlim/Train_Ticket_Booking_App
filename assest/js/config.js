3fe9a121b7d21b2fb0fd21653b3fce403d02303c
SohailAlim <sohailalim311@outlook.com>
2025-10-07 14:36:53 +0530

    added env

---
+++ assest/js/config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+
+  apiKey: "FAPIK",
+  authDomain: "irctc-1.firebaseapp.com",
+  databaseURL: "https://irctc-1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "irctc-1",
+  storageBucket: "irctc-1.firebasestorage.app",
+  messagingSenderId: "429642366857",
+  appId: "1:429642366857:web:3aedabfb00d4e58d326b58"
+
+}
